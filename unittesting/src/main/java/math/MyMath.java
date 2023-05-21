46bfab9427ce663243768c9e9069d4f6160898a0
NikosNikolaidis02 <t8200120@aueb.gr>
2023-05-21 21:04:24 +0300

    Create test case for the class Arithmetic Operations

---
+++ unittesting/src/main/java/math/MyMath.java
@@ -0,0 +1,12 @@
+package math;
+
+/**
+ * This is a Java class that contains a method that computes the factorial of a number n
+ *
+ * @author NikosNikolaidis02
+ * @version 1.0
+ * @since 2023-05-21
+ */
+public class MyMath {
+    public
+}
