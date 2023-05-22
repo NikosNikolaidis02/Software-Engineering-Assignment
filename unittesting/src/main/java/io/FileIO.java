a991a6dd270f314cea69c5875c1161c42fe1c3fb
NikosNikolaidis02 <92633955+NikosNikolaidis02@users.noreply.github.com>
2023-05-22 21:08:00 +0300

    Add files via upload

--- unittesting/src/main/java/io/FileIO.java
+++ unittesting/src/main/java/io/FileIO.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
-* The FileIO provides simple file input/output operations
-* that serve as hands-on practice on Unit Testing.
-*
-* @author  agkortzis
-* @version 1.0
-* @since   2020-04-06 
-*/
+ * The FileIO provides simple file input/output operations
+ * that serve as hands-on practice on Unit Testing.
+ *
+ * @author  agkortzis
+ * @version 1.0
+ * @since   2020-04-06
+ */
 public class FileIO {
-	
+
 	/**
 	 * Reads a file that contains numbers line by line 
 	 * and returns an array of the integers found in the file.
@@ -29,25 +29,31 @@ public class FileIO {
 	 */
 	public int[] readFile(String filepath) {
 		File file = new File(filepath);
-		if (!file.exists()) 
+		if (!file.exists())
 			throw new IllegalArgumentException("Input file does not exist");
-		
+
 		List<Integer> numbersList = new ArrayList<>();
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				int number = Integer.parseInt(line);
-				numbersList.add(number);
+				int number = 0;
+				try {
+					int num = Integer.parseInt(line);
+					number = num;
+					numbersList.add(number);
+				} catch (Exception e) {
+					throw new IllegalArgumentException("Wrong Input values");
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if (numbersList.size() == 0) 
+
+		if (numbersList.size() == 0)
 			throw new IllegalArgumentException("Given file is empty");
-		
+
 		// Convert a List to an array using 
 		return numbersList.stream().mapToInt(i -> i).toArray();
 	}
