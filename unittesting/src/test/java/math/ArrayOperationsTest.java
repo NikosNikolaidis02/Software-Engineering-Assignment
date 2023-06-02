5a28a15550f93bd79c300cfa2964712189968784
NikosNikolaidis02 <t8200120@aueb.gr>
2023-06-03 01:05:04 +0300

    Resolving issue #19

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -2,9 +2,12 @@ package math;
 
 import io.FileIO;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 
 import java.io.File;
@@ -21,20 +24,36 @@ public class ArrayOperationsTest {
 
     ArrayOperations arrayOperationsObj = new ArrayOperations();
 
+    @Mock
+    private FileIO fileIO;
+
+    @Mock
+    private MyMath myMath;
+
+    @Before
+    public void setUp() {
+        fileIO = Mockito.mock(FileIO.class);
+        myMath = Mockito.mock(MyMath.class);
+    }
 
     /**
      * Tests the result of the function findPrimesInFile of the class Array Operations
      */
     @Test
     public void testFindPrimesInFile() {
-        MyMath myMathObj = mock(MyMath.class);
-        FileIO fileIO = mock(FileIO.class);
 
         String resourcesPath = "unittesting/src/test/resources/";
         String filepath = resourcesPath + "primes.txt";
+        int[] input = {2, 3, 4, 5, 6};
+        int[] result = {2,3,5};
 
-        int[] result = new int[]{2,3,5};
+        Mockito.when(fileIO.readFile(filepath)).thenReturn(input);
+        Mockito.when(myMath.isPrime(2)).thenReturn(true);
+        Mockito.when(myMath.isPrime(3)).thenReturn(true);
+        Mockito.when(myMath.isPrime(4)).thenReturn(false);
+        Mockito.when(myMath.isPrime(5)).thenReturn(true);
 
-        //Assert.assertArrayEquals(result,arrayOperationsObj.findPrimesInFile(fileIO, filepath, myMathObj));
+
+        Assert.assertArrayEquals(result,arrayOperationsObj.findPrimesInFile(fileIO, filepath, myMath));
     }
 }
